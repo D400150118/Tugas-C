409de6310af059b1270c9c6f8bb3fcffeec2dac1
D400150102 <krisma_verryn@yahoo.co.id>
2015-09-28 11:02:18 +0700

    tugas-02

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{
+float a,b,c,D,x,x1,x2;
+printf("Program Mencari Akar Persamaan Kuadrat\n");
+printf("Bentuk Persamaan ax^2 + bx + c = 0\n");
+printf("-------------------------------------------- \n");
+printf(" Masukan Nilai Koefisien a = ");scanf("%f",&a);
+printf(" Masukan Nilai Koefisien b = ");scanf("%f",&b);
+printf(" Masukan Nilai Koefisien c = ");scanf("%f",&c);
+printf("-------------------------------------------- \n");
+
+D = (b*b)-(4*a*c);
+printf(" Diskriminan = %2.2f\n",D);
+printf("-------------------------------------------- \n");
+if (D>0)
+        {x1=((-b+sqrt(D))/(2*a));
+                printf(" x1 = %2.2f\n",x1);
+         x2=((-b-sqrt(D))/(2*a));
+                printf(" x2 = %2.2f\n",x2);}
+else if(D==0)
+        {x= -b/(2*a);
+                printf(" x1 = x2 = %2.2f\n",x);}
+else if(D<0)
+        {x1=((-b/2*a)+ ((sqrt(-D))/2*a));
+                printf(" x1 = %2.2f\n",x1);
+         x2=((-b/2*a)-((sqrt(-D) )/2*a));
+                printf(" x2 = %2.2f\n",x2);
+                printf(" Adalah Bilangan Imajiner \n");}
+printf("--------------------------------------------\n");
+
+getch();
+}
